25dd60f0b1ddda44170b6bf54bcdfb65019753e2
Helena Rasche <hxr@hx42.org>
2021-02-09 18:32:56 +0100

    add missing tools

--- bin/build-tool-check.sh
+++ bin/build-tool-check.sh
@@ -11,16 +11,17 @@ for directory in $(./bin/extract-tutorials.sh); do
 			echo "  ## $ga" >> tools.yml
 			grep 'toolshed.g2.bx.psu.edu/repos[^"]*' -o  "$ga" | sort -u | sed 's/^/  - /g' >> tools.yml
 		done
-	else
-		tool_ids="$(cat "$tutorial" | grep '{% tool' | sed 's/%}.*//g' | grep '(toolshed.*)' -o | sed -e 's/(\|)//g')"
-		lines=$(echo "$tool_ids" | wc -l)
-		if (( lines > 1 )); then
-			for tool_id in $tool_ids; do
-				echo "$tool_id" | sed 's/^/  - /g' >> tools.yml
+	fi
 
-			done
-		else
-			echo "$tutorial missing tool IDs"
-		fi
+	echo "  ## tools" >> tools.yml
+	tool_ids="$(cat "$tutorial" | grep '{% tool' | sed 's/%}.*//g' | grep '(toolshed.*)' -o | sed -e 's/(\|)//g' | sort -u)"
+	lines=$(echo "$tool_ids" | wc -l)
+	if (( lines > 1 )); then
+		for tool_id in $tool_ids; do
+			echo "$tool_id" | sed 's/^/  - /g' >> tools.yml
+
+		done
+	else
+		echo "$tutorial missing tool IDs"
 	fi
 done
